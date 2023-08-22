eb70d36450a0e282173b3e5a867c0485744ad7f8
Theo Tacail <theo.tacail@gmail.com>
2023-08-22 17:07:08 +0200

    updated pipe and tidyeval namespace management

--- isobxr/R/isobxr-package.R
+++ isobxr/R/isobxr-package.R
@@ -3,5 +3,11 @@
 
 ## usethis namespace: start
 #' @importFrom magrittr %>%
+#' @importFrom rlang :=
+#' @importFrom rlang .data
+#' @importFrom rlang as_label
+#' @importFrom rlang as_name
+#' @importFrom rlang enquo
+#' @importFrom rlang enquos
 ## usethis namespace: end
 NULL
